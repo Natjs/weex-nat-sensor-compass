97379e209b523c7e6edc61c9228d4a43717b1a78
黄亚克 <13323829533@163.com>
2017-02-16 13:34:47 +0800

    0.0.1

---
+++ ios/NatWeexCompass.m
@@ -0,0 +1,63 @@
+//
+//  NatWeexCompass.m
+//
+//  Created by huangyake on 17/1/7.
+//  Copyright © 2017 Nat. All rights reserved.
+//
+
+
+#import "NatWeexCompass.h"
+#import "NatCompass.h"
+
+@implementation NatWeexCompass
+WX_EXPORT_METHOD(@selector(get:))
+WX_EXPORT_METHOD(@selector(watch::))
+WX_EXPORT_METHOD(@selector(clearWatch:))
+
+- (void)get:(WXModuleCallback)callback{
+    [[NatCompass singletonManger] get:^(id error,id result) {
+        if (error) {
+            if (callback) {
+                callback(error);
+            }
+        }else{
+            if (callback) {
+                callback(result);
+            }
+        }
+    }];
+}
+
+- (void)watch:(NSDictionary *)options :(WXKeepAliveCallback)callback{
+    [[NatCompass singletonManger] watch:options :^(id error,id result) {
+        if (error) {
+            if (callback) {
+                callback(error,YES);
+            }
+        }else{
+            if (callback) {
+                callback(result,YES);
+            }
+        }
+    }];
+}
+
+- (void)clearWatch:(WXModuleCallback)callback{
+    [[NatCompass singletonManger] clearWatch:^(id error,id result) {
+        if (error) {
+            if (callback) {
+                callback(error);
+            }
+        }else{
+            if (callback) {
+                callback(result);
+            }
+        }
+    }];
+}
+
+- (void)dealloc{
+    [[NatCompass singletonManger] close];
+}
+
+@end
